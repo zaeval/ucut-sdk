b1b2ddc72c1b88af880c3fabce0aa54223ce1293
zaeval <zaeval@108.software>
2023-01-03 18:57:01 +0900

    ucut-python-sdk init

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
 # This is a sample Python script.
+import requests
+from requests import Response
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class UcutManager:
+    BASE_URL = "https://service.ucut.in/api/shorturl"
+
+    def __init__(self, api_key: str):
+        self._api_key = api_key
+
+    def __str__(self):
+        return "< UcutManager: {} >".format(self._api_key)
+
+    def __repr__(self):
+        return "< UcutManager: {} >".format(self._api_key)
+
+    # TODO: link 생성 api 연동 sdk
+    def createLink(self,
+                   origin: str,
+                   key: str = None,
+                   alias: str = None,
+                   author: int = None,
+                   private: bool = None,
+                   password: str = None) -> Response:
+        return requests.post("{BASE_URL}/{PATH}/".format(
+            BASE_URL=self.BASE_URL, PATH="/link"
+        ),
+            headers={
+                'X-Api-Key': self._api_key
+            },
+            data={
+                "key": key,
+                "alias": alias,
+                "author": author,
+                "origin": origin,
+                "private": private,
+                "password": password
+            })
+
+    # TODO: link 가져오기 sdk
+    def getLink(self, key: str = None, alias: str = None, origin: str = None):
+        params = {}
+        if key != None:
+            params["key"] = key
+        if alias != None:
+            params["alias"] = alias
+        if origin != None:
+            params["origin"] = origin
+
+        return requests.get("{BASE_URL}/{PATH}/".format(
+            BASE_URL=self.BASE_URL, PATH="link"
+        ),
+            headers={
+                'X-Api-Key': self._api_key
+            },
+            params=params)
+
+    # TODO: authentication 요청 연동 sdk
+    def authentication(self, id: str, password: str):
+        return requests.get("{BASE_URL}/{PATH}/".format(
+            BASE_URL=self.BASE_URL, PATH="link/{ID}/authentication".format(ID=id)
+        ),
+            headers={
+                'X-Api-Key': self._api_key,
+                'X-Password': password
+            })
 
 
-#TODO: link 생성 api 연동 sdk
-#TODO: authentication 요청 연동 sdk
